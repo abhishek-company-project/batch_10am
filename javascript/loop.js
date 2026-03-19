9096ac53a23a905c963268a308d92c5adbef1b4b
abhishek-company-project <abhishek.shantiinfosoft@gmail.com>
2026-03-19 14:30:02 +0530

    ad

--- javascript/loop.js
+++ javascript/loop.js
@@ -36,3 +36,89 @@
 //     }
 // }
 
+
+
+// for(var i=15;i>5;i-=5){
+//     console.log("helllo")
+// }
+
+// var pattern="";
+
+// for(var i=0;i<3;i++){
+//     for(var j=0;j<4;j++){
+//         if(i==0 || i==2 || j==0 ||j==3){
+//             pattern+="* "
+//         }else{
+//             pattern+="  "
+//         }
+
+//     }
+//     pattern+="\n"
+// }
+
+
+// var num=10
+// for(var i=0;i<num;i++){
+
+//     for(k=0;k<num-1-i;k++){
+//         pattern+="  "
+//     }
+
+//     for(var j=0;j<i+1;j++){
+//         pattern+="* "
+//     }
+//     pattern+="\n"
+// }
+
+// console.log(pattern)
+
+
+
+// var str="my name is abhishek"
+
+// var newstr="";
+// var is_space=false;
+
+// for(var i=0;i<str.length;i++){
+//     if(i==0 || is_space==true){
+//         newstr+=str[i].toUpperCase();
+//         is_space=false;
+//         continue;
+//     }
+//     if(str[i]==" "){
+//         is_space=true;
+//         newstr+=str[i];
+//     }
+//     if(str[i]!=" "){
+//         newstr+=str[i];
+//     }
+
+// }
+
+// console.log(newstr)
+
+// var str="banana";
+
+// b1a3n2
+
+
+
+
+// var person={
+//     name:"abhi",
+//     surname:"gurjar",
+//     age:25,
+//     getAllDetails:function(){
+//         return this.name + this.surname
+//     }
+// }
+
+
+// function abc(){
+//     console.log("hello")
+// }
+
+// abc();
+
+// console.log(person.getAllDetails());
+
---
+++ javascript/object.js
@@ -0,0 +1,21 @@
+
+
+// var person={
+//     name:"abhishek",
+//     surname:"gurjar",
+//     age:25,
+//     college:"lnct",
+//     company:"shanti infosoft"
+// }
+
+// var {name,age,company}=person;
+
+// console.log(age)
+
+
+try{
+    console.log(a)
+}
+catch(err){
+    console.log(err.name)
+}
